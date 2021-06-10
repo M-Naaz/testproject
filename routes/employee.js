a020b49ed1fe12bf36d1b93d43b0f1d2b7937bcd
M-Naaz <84713195+M-Naaz@users.noreply.github.com>
2021-06-10 16:34:29 +0530

    Update employee.js

--- routes/employee.js
+++ routes/employee.js
@@ -6,11 +6,14 @@ const upload = require("../middleware/upload")
 
 const authenticate = require("../middleware/authenticate")
 
-router.get("/, EmployeeController.index")
-router.post("/show", EmployeeController.show)
-router.post("/store", upload.array("profileImage[]"),EmployeeController.store)
-router.post("/update", EmployeeController.update)
-router.post("/delete", EmployeeController.delete)
+router.get('/hi', (req, res, next) => {
+    res.send('Welcome to Home Page')
+  });
+// router.get("/, EmployeeController.index")
+// router.post("/show", EmployeeController.show)
+// router.post("/store", upload.array("profileImage[]"),EmployeeController.store)
+// router.post("/update", EmployeeController.update)
+// router.post("/delete", EmployeeController.delete)
 
 
 module.exports = router
